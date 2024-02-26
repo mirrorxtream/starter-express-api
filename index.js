ddc9bc315b509ab606259cf34e6500a9cbb0487a
Wezure <129850821+mirrorxtream@users.noreply.github.com>
2024-02-27 00:34:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const proxy = require('http-proxy-middleware');
+
 const app = express();
 
-// Add CORS headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+const port = process.env.PORT || 3000;
+
+const onProxyReq = function (proxyReq, req, res) {
+  proxyReq.setHeader('Authorization', 'Basic SECRET');
+};
+
+const apiProxy = proxy('**', {
+    target: 'https://west.biz.id',
+    changeOrigin: true, // for vhosted sites
+    onProxyReq: onProxyReq
 });
 
-app.get('/api', async (req, res) => {
-    try {
-        const { url } = req.query;
+app.use(cors());
+app.use(apiProxy);
 
-        // Error Handling
-        if (!url) {
-            return res.status(400).json({ error: 'Missing URL parameter' });
-        }
-
-        try {
-            new URL(url);
-        } catch (error) {
-            return res.status(400).json({ error: 'Invalid URL' });
-        }
-
-        try {
-            await fetch(url, { method: 'HEAD' });
-        } catch (error) {
-            return res.status(400).json({ error: "Failed to fetch" });
-        }
-        
-        // Make a request and send response back to client
-        const response = await fetch(url);
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-})
-
-app.all('/', (req, res) => {
-    res.send('OK');
-})
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+app.listen(port, function() {
+	console.log('Proxy app is running on http://localhost:' + port);
+});
